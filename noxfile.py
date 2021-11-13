dbebcb2ca19570785acdef2d73c9845bfb11b694
Diego Ramirez <dr01191115@gmail.com>
2021-11-13 11:46:27 -0600

    Add sessions to our noxfile
    Generate an uncompromising DiddiScript console, installed on the Nox's virtualenv and using the latest DiddiParser2 code. Use it for testing purposes only.
    
    ----
    
    Also, I added some docstrings and hints.

--- noxfile.py
+++ noxfile.py
@@ -13,8 +13,25 @@ def lint(session):
 
 @nox.session
 def format(session):
-    "Run black on the codebase."
+    """
+    Run black and isort on the codebase. It is
+    known that both formatters can have conflicts,
+    so try to run `nox -s lint` before commiting!
+    """
     session.install("-r", "tests/requirements.txt")
-    session.install("-e", ".")
     session.run("isort", ".")
     session.run("black", ".")
+    session.log("To get sure both formatters are happy, run 'nox -s lint'.")
+
+
+@nox.session(name="generate-console")
+def generate_console(session):
+    """
+    Generate an uncompromising DiddiScript console,
+    installed on the Nox's virtualenv and using the
+    latest DiddiParser2 code. Use it for testing purposes
+    only.
+    """
+    session.install("-e", ".")
+    session.log("Generating a DiddiScript console...")
+    session.run("diddiscript-console")
