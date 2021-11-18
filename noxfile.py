d6cfc0c59b4e8a90fc2db1f4f476a9999eb0744c
Diego Ramirez <dr01191115@gmail.com>
2021-11-18 16:49:42 -0600

    Add stuff to our noxfile
    Combined sessions, docstrings, logging, etc.

--- noxfile.py
+++ noxfile.py
@@ -14,8 +14,8 @@ def lint(session):
 @nox.session
 def format(session):
     """
-    Run black and isort on the codebase. It is
-    known that both formatters can have conflicts,
+    Run black and isort on the codebase.
+    It is known that both formatters can have conflicts,
     so try to run `nox -s lint` before commiting!
     """
     session.install("-r", "tests/requirements.txt")
@@ -24,11 +24,33 @@ def format(session):
     session.log("To get sure both formatters are happy, run 'nox -s lint'.")
 
 
+@nox.session(name="format-and-lint")
+def format_and_lint(session):
+    """
+    Run both formatters and linters.
+    It should not fail, unless one of the
+    linters have a conflict with another
+    linter.
+    """
+    session.warn(
+        "We'll run formatters and linters together. "
+        "However, conflicts between linters may be caused by this session."
+    )
+    session.install("--upgrade", "nox")
+    session.log("Formatting...")
+    session.run("nox", "-s", "format")
+    session.log(
+        "Running linters (this part of the session may cause conflicts)."
+    )
+    session.run("nox", "-s", "lint")
+    session.log("It seems like everything succeeded!")
+
+
 @nox.session(name="generate-console")
 def generate_console(session):
     """
-    Generate an uncompromising DiddiScript console,
-    installed on the Nox's virtualenv and using the
+    Generate an uncompromising DiddiScript console.
+    It is installed on the Nox's virtualenv and using the
     latest DiddiParser2 code. Use it for testing purposes
     only.
     """
@@ -41,7 +63,7 @@ def generate_console(session):
 def release(session):
     """
     Just for maintainers: cut a release and publish to PyPI
-    using Setuptools, Wheel and Twine
+    using Setuptools, Wheel and Twine.
     """
     # install deps
     session.install("-r", "release-requirements.txt")
