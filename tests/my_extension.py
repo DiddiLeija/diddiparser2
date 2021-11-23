0103f2ab4c4bf0a9d3d7a74cf1c1de4d904875fe
Diego Ramirez <dr01191115@gmail.com>
2021-11-23 12:54:53 -0600

    Create my_extension.py
    A simple extension file, used by our tests.

---
+++ tests/my_extension.py
@@ -0,0 +1,12 @@
+"""
+Test extensions file.
+"""
+
+DIDDISCRIPT_FUNCTIONS = ("my_func")
+
+
+def my_func(arg):
+    """
+    A simple DiddiScript function written in Python.
+    """
+    print(f"Hi! You gave me '{arg}' as an argument...")
