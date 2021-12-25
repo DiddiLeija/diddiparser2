679b665b427bf7cf55a1c56ed28675ab9da4e1b4
Diego Ramirez <dr01191115@gmail.com>
2021-12-25 10:15:53 -0600

    Start with the DiddiScript editor

---
+++ diddiparser2/editor/__init__.py
@@ -0,0 +1,8 @@
+"""
+The DiddiScript code editor.
+
+This editor is based in the Python IDLE,
+built with Tkinter. We decided to build it with
+Tkinter and idlelib to avoid 'additional deps'
+and to keep minimal.
+"""
