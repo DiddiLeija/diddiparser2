425f74c68acd0c70bcead29a1a581b624a39cc95
Diego Ramirez <dr01191115@gmail.com>
2022-01-09 13:54:07 -0600

    Prepare docs to have semantic versioning

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,13 @@
 # Sphinx config file.
+import sys
+import os
+
+try:
+    import importlib.metadata as metadata
+except ImportError:
+    import importlib_metadata as metadata
+
+sys.path.insert(0, os.path.abspath(".."))
 
 # we want some extensions, so we are including them here:
 extensions = [
@@ -15,6 +24,9 @@ language = "en"
 project = "DiddiParser2"
 author = "Diego Ramirez"
 copyright = "2021, Diego Ramirez"
+
+version = metadata.version("diddiparser2")
+release = version
 
 # HTML specs
 html_theme = "furo"
