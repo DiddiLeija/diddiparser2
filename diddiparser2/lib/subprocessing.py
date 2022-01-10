dd7ca18a06739dc8b1812785d65091694f5390de
Diego Ramirez <dr01191115@gmail.com>
2022-01-10 14:05:15 -0600

    Improve the `subprocessing` library
    Also, I reworked a function.

--- diddiparser2/lib/subprocessing.py
+++ diddiparser2/lib/subprocessing.py
@@ -2,49 +2,38 @@
 Subprocessing: Make subprocesses as DiddiScript functions.
 """
 
-import shlex
 import subprocess
 import sys
-import traceback
 
 from diddiparser2.messages import run_error
 
-DIDDISCRIPT_FUNCTIONS = ("run_command", "run_python_code")
-
-# TODO: Find a better way to define PYTHON_GLOBALS.
-# By default, the globals are the same than
-# the globals(), but the locals are a bit
-# different.
-PYTHON_GLOBALS = globals()
-PYTHON_LOCALS = {"__name__": "__console__", "__doc__": None}
+DIDDISCRIPT_FUNCTIONS = ("run_command", "run_python_cmd")
 
 
-def run_command(cmd):
+def build_command(*args):
+    "Build and format a tuple of commands and return a string."
+    final = ""
+    for pos in range(len(args)):
+        final += arg
+        final += " " if pos != len(args) - 1 else ""
+    return final
+
+
+def run_command(*cmd):
     "Run a command, as a subprocess"
     try:
-        parsed_cmd = shlex.split(cmd)
-        subprocess.run(parsed_cmd, shell=True)
+        subprocess.run(*cmd)
     except Exception as exc:
         run_error(
-            f"The subprocess '{cmd}' failed with error: '{str(exc)}' ({type(exc).__name__})"
+            f"The subprocess '{build_command(*cmd)}' failed with error: '{str(exc)}' ({type(exc).__name__})"
         )
 
 
-def run_python_code(cmd):
-    "Execute a line of Python code."
+def run_python_cmd(*args):
+    "Execute a Python command, as '{python executable} {arguments}'"
     try:
-        # execute the command as-is
-        exec(cmd, PYTHON_GLOBALS, PYTHON_LOCALS)
-    except KeyboardInterrupt:
-        run_error("Operation interrupted by user")
-    except Exception:
-        # Simulate an exception, but using fancy
-        # DiddiParser functions.
-        exc_type, value, tb = sys.exc_info()
-        sys.last_type = exc_type
-        sys.last_value = value
-        sys.last_traceback = tb
+        subprocess.run([sys.executable] + list(*args))
+    except Exception as exc:
         run_error(
-            f"The Python code '{cmd}' raised this error:"
-            f"\n{traceback.format_exception(exc_type, value, sys.last_traceback)}"
+            f"The subprocess '{build_command(*args)}' failed with error: '{str(exc)}' ({type(exc).__name__})"
         )
