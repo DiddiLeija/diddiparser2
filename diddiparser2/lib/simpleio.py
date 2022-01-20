0980a097f18566ae30fbd75e80ea4d775db806ce
Diego Ramirez <dr01191115@gmail.com>
2022-01-20 12:53:33 -0600

    Improve `simpleio.store_input`

--- diddiparser2/lib/simpleio.py
+++ diddiparser2/lib/simpleio.py
@@ -6,26 +6,10 @@ SimpleIO: Common Python I/O interactions
 
 import sys
 
-from diddiparser2.diddiscript_types import Null, Text
+from diddiparser2.diddiscript_types import Text
 from diddiparser2.messages import run_error
 
 DIDDISCRIPT_FUNCTIONS = ("program_exit", "print_text", "print_line", "store_input")
-
-
-class TextContainer:
-    "Keep the inputs, and retrieve them."
-
-    def __init__(self):
-        self.text = Null()
-
-    def store_text(self, text):
-        self.text = Text(text)
-
-    def get_text(self):
-        return self.text
-
-
-INPUTS = TextContainer()
 
 
 def program_exit(msg):
@@ -53,5 +37,4 @@ def store_input(msg):
         text = input(msg)
     except KeyboardInterrupt:
         run_error("Input request was interrupted by user!")
-    INPUTS.store_text(text)
-    return INPUTS.get_text()
+    return Text(text)
